b345ed4082df267fa0245a0bbf9e39df738ddfaf
Alec Cunningham <aleccunningham96@gmail.com>
2017-12-15 14:43:26 -0600

    Update provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
-export COMPOSE_PROJECT_NAME=analyte
-/usr/bin/apt-get update > /dev/null 2>&1
-/usr/bin/apt-get install -y git software-properties-common \
-  python-software-properties python python-dev > /dev/null 2>&1
-wget -qO- https://get.docker.com/ | sh > /dev/null 2>&1
-wget \
-  -O /usr/local/bin/docker-compose \
-  https://github.com/docker/compose/releases/download/1.17.1/run.sh \
-&& chmod +x /usr/local/bin/docker-compose > /dev/null 2>&1
-usermod -aG docker $USER > /dev/null 2>&1
-docker network create --driver bridge analyte
-docker volume create --name db-data
-docker-compose
-# git clone $BRANCH_SLUG
-gcloud auth activate-service-account --key-file /vagrant/creds/registry-access.json
+/usr/bin/apt-get update 
+/usr/bin/apt-get install -y git software-properties-common
+/usr/bin/wget -qO- https://get.docker.com/ | sh
+/usr/bin/wget -O /usr/local/bin/docker-compose \
+  https://github.com/docker/compose/releases/download/1.17.1/run.sh
+/usr/bin/chmod +x /usr/local/bin/docker-compose
+/usr/bin/wget -O /usr/local/bin/docker-compose -L \ 
+  "https://github.com/docker/compose/releases/download/1.17.1/docker-compose-$(uname -s)-$(uname -m)"
+usermod -aG docker $USER
